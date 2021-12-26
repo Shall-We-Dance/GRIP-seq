05c238a7b46a23ca86bb445961b5d5f3b334d1e5
ShallWeTalk <91458979+Shall-We-Dance@users.noreply.github.com>
2021-12-25 17:18:48 -0800

    Update generate_genome_index.sh

--- scripts/generate_genome_index.sh
+++ scripts/generate_genome_index.sh
@@ -14,15 +14,20 @@ fi
 GENOME_DIR="$1"
 CPU_THREADS=${2:-"8"}  # Default using 8 threads
 INDEX_LENGTH=${3:-"100"}  # Default using 100 as the index length
+
+echo "Genome dir: ${GENOME_DIR} ..."
+echo "--runThreadN: ${CPU_THREADS} ..."
+echo "--sjdbOverhang: ${INDEX_LENGTH} ..."
+
 # activate enviroment:
 source ~/.bashrc
 
 mkdir -p ${GENOME_DIR}/hg19
 
-echo "Downloading Fasta File to ${DOWNLOAD_MODE} ..."
+echo "Downloading Fasta File to ${GENOME_DIR} ..."
 wget -P ${GENOME_DIR} https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/GRCh37.p13.genome.fa.gz
 
-echo "Downloading GTF File to ${DOWNLOAD_MODE} ..."
+echo "Downloading GTF File to ${GENOME_DIR} ..."
 wget -P ${GENOME_DIR} https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.chr_patch_hapl_scaff.annotation.gtf.gz
 
 echo "Running STAR to generate genome index ..."
