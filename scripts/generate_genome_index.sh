f4f77763419ae9737a1fe4f6b267bd7ba293d923
Liu <91458979+Shall-We-Dance@users.noreply.github.com>
2021-12-28 17:56:10 -0800

    Update generate_genome_index.sh

--- scripts/generate_genome_index.sh
+++ scripts/generate_genome_index.sh
@@ -27,8 +27,14 @@ mkdir -p ${GENOME_DIR}/hg19
 echo "Downloading Fasta File to ${GENOME_DIR} ..."
 wget -P ${GENOME_DIR}/hg19 https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/GRCh37.p13.genome.fa.gz
 
+echo "Unzipping Fasta File"
+gzip -d ${GENOME_DIR}/hg19/GRCh37.p13.genome.fa.gz
+
 echo "Downloading GTF File to ${GENOME_DIR} ..."
 wget -P ${GENOME_DIR}/hg19 https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_19/gencode.v19.chr_patch_hapl_scaff.annotation.gtf.gz
+
+echo "Unzipping GTF File"
+gzip -d ${GENOME_DIR}/hg19/gencode.v19.chr_patch_hapl_scaff.annotation.gtf.gz
 
 echo "Running STAR to generate genome index ..."
 mkdir -p ${GENOME_DIR}/STAR_index
