63c95ceb90ff773ab7d40f36772eeec45ce56b21
Liu <91458979+Shall-We-Dance@users.noreply.github.com>
2021-12-29 02:22:45 -0800

    Update peaks.R
    1. If sth == NA, sth <- 5, to fix a problem caused by only use depth > 10 coverage (samtools depth)
    2. A broader window, 5bp -> 8bp

--- peaks/peaks.R
+++ peaks/peaks.R
@@ -29,21 +29,21 @@ for(i in 1:nrow(raw)){
   if (raw[i,4] == '-'){
     temp_high_3 <- data.frame(raw[i,1],raw[i,3]-3)
     colnames(temp_high_3) <- c("chr","postion")
-    temp_high_5 <- data.frame(raw[i,1],raw[i,3]-5)
+    temp_high_5 <- data.frame(raw[i,1],raw[i,3]-8)
     colnames(temp_high_5) <- c("chr","postion")
     temp_low_3 <- data.frame(raw[i,1],raw[i,3]+3)
     colnames(temp_low_3) <- c("chr","postion")
-    temp_low_5 <- data.frame(raw[i,1],raw[i,3]+5)
+    temp_low_5 <- data.frame(raw[i,1],raw[i,3]+8)
     colnames(temp_low_5) <- c("chr","postion")
   }
   if (raw[i,4] == '+'){
     temp_high_3 <- data.frame(raw[i,1],raw[i,3]+3)
     colnames(temp_high_3) <- c("chr","postion")
-    temp_high_5 <- data.frame(raw[i,1],raw[i,3]+5)
+    temp_high_5 <- data.frame(raw[i,1],raw[i,3]+8)
     colnames(temp_high_5) <- c("chr","postion")
     temp_low_3 <- data.frame(raw[i,1],raw[i,3]-3)
     colnames(temp_low_3) <- c("chr","postion")
-    temp_low_5 <- data.frame(raw[i,1],raw[i,3]-5)
+    temp_low_5 <- data.frame(raw[i,1],raw[i,3]-8)
     colnames(temp_low_5) <- c("chr","postion")
   }
   temp <- temp_high_3
@@ -52,44 +52,52 @@ for(i in 1:nrow(raw)){
   temp <- bind_rows(temp,temp_low_5)
   temp <- left_join(temp,ref_chr,by=c("chr","postion"))
   #get depth
-  #temp_high <- left_join(temp_high,ref_chr,by=c("chr","postion"))
-  #temp_low <- left_join(temp_low,ref_chr,by=c("chr","postion"))
-  if(is.na(temp[1,3]) | is.na(temp[2,3]) | is.na(temp[3,3]) | is.na(temp[4,3])){
-  }
-  else{
-    if(temp[1,3]/temp[3,3] > threshhold | temp[2,3]/temp[4,3] > threshhold | temp[1,3]/temp[4,3] > threshhold | temp[2,3]/temp[3,3] > threshhold){
-      if (raw[i,4] == '-'){
-        temp_peak <- data.frame(V1=raw[i,1],V2=(raw[i,3]-5):(raw[i,3]+5))
-        colnames(temp_peak) <- c("chr","postion")
-        temp_peak <- left_join(temp_peak,ref,by=c("chr","postion"))
-        for (j in 1:(nrow(temp_peak)-1)){
-          if(is.na(temp_peak[j+1,3]) | is.na(temp_peak[j,3])){
-          }
-          else{
-            if(temp_peak[j,3]/temp_peak[j+1,3] > threshhold_peak){
-              temp_result = data.frame(raw[i,1],(temp_peak[j,2]-1),(temp_peak[j+1,2]-1),raw[i,4])
-              colnames(temp_result) <- c("chr","start","end","strain")
-              result = bind_rows(result,temp_result)
-              break
-            }
-          }
+  #is.na
+  for( k in 1:4){
+    if(is.na(temp[i,4])){
+    temp[i,4] <- 5
+    }
+  } 
+  if(temp[1,3]/temp[3,3] > threshhold | temp[2,3]/temp[4,3] > threshhold | temp[1,3]/temp[4,3] > threshhold | temp[2,3]/temp[3,3] > threshhold){
+    if (raw[i,4] == '-'){
+      temp_peak <- data.frame(V1=raw[i,1],V2=(raw[i,3]-8):(raw[i,3]+8))
+      colnames(temp_peak) <- c("chr","postion")
+      temp_peak <- left_join(temp_peak,ref,by=c("chr","postion"))
+      for (j in 1:(nrow(temp_peak)-1)){
+        #is.na
+        if(is.na(temp_peak[j,3])){
+          temp_peak[j,3] <- 5
+        }
+        #is.na
+        if(is.na(temp_peak[j+1,3])){
+          temp_peak[j+1,3] <- 5
+        }
+        if(temp_peak[j,3]/temp_peak[j+1,3] > threshhold_peak){
+          temp_result = data.frame(raw[i,1],(temp_peak[j,2]-1),(temp_peak[j+1,2]-1),raw[i,4])
+          colnames(temp_result) <- c("chr","start","end","strain")
+          result = bind_rows(result,temp_result)
+          break
         }
       }
-      if (raw[i,4] == '+'){
-        temp_peak <- data.frame(V1=raw[i,1],V2=(raw[i,2]-5):(raw[i,2]+5))
-        colnames(temp_peak) <- c("chr","postion")
-        temp_peak <- left_join(temp_peak,ref,by=c("chr","postion"))
-        for (j in 1:(nrow(temp_peak)-1)){
-          if(is.na(temp_peak[j+1,3]) | is.na(temp_peak[j,3])){
-          }
-          else{
-            if(temp_peak[j+1,3]/temp_peak[j,3] > threshhold_peak){
-              temp_result = data.frame(raw[i,1],temp_peak[j,2],temp_peak[j+1,2],raw[i,4])
-              colnames(temp_result) <- c("chr","start","end","strain")
-              result = bind_rows(result,temp_result)
-              break
-            }
-          }
+    }
+    if (raw[i,4] == '+'){
+      temp_peak <- data.frame(V1=raw[i,1],V2=(raw[i,2]-8):(raw[i,2]+8))
+      colnames(temp_peak) <- c("chr","postion")
+      temp_peak <- left_join(temp_peak,ref,by=c("chr","postion"))
+      for (j in 1:(nrow(temp_peak)-1)){
+        #is.na
+        if(is.na(temp_peak[j,3])){
+          temp_peak[j,3] <- 5
+        }
+        #is.na
+        if(is.na(temp_peak[j+1,3])){
+          temp_peak[j+1,3] <- 5
+        }
+        if(temp_peak[j+1,3]/temp_peak[j,3] > threshhold_peak){
+          temp_result = data.frame(raw[i,1],temp_peak[j,2],temp_peak[j+1,2],raw[i,4])
+          colnames(temp_result) <- c("chr","start","end","strain")
+          result = bind_rows(result,temp_result)
+          break
         }
       }
     }
