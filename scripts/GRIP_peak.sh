7eef77d706fcd52b1da62b655f8b69807266633c
Liu <91458979+Shall-We-Dance@users.noreply.github.com>
2021-12-31 14:26:49 -0800

    Update GRIP_peak.sh

--- scripts/GRIP_peak.sh
+++ scripts/GRIP_peak.sh
@@ -1,14 +1,53 @@
-ROOTDIR=.
-for NAME in WS013 WS014 WS015 WS016
+#!/bin/bash
+
+#Find peaks for GRIP-seq
+
+#GRIP-seq pipeline
+#Author: Hongijang Liu
+#Email: hongjiang.liu@ucsf.edu
+
+# Exit the script if an error happens
+set -e
+
+if [[ $# -eq 0 ]]; then
+  echo "Error: ROOTDIR must be provided as an input argument."
+  exit 1
+fi
+
+REPEAT=$(cat ./repeat.txt)
+
+ROOTDIR="$1"
+ID=${2:-"current"}  # Default ID is current
+THREAD=${3:-"8"}  # Default using 8 threads
+
+echo "ROOTDIR: ${ROOTDIR} ..."
+echo "ID: ${ID} ..."
+
+echo "Making dir for peak..."
+mkdir -p ${ROOTDIR}/peak/${ID}
+
+echo "Preparing data form clipper..."
+for NAME in ${REPEAT};
 do
-#call_summits
-zcat ${ROOTDIR}/${NAME}.peak.bed.gz |  awk -F"\t" '{if ($6=="+") print $1"\t"$2"\t"$2+1"\t"$4"\t"$5"\t"$6; else print $1"\t"$3-1"\t"$3"\t"$4"\t"$5"\t"$6}' > ${ROOTDIR}/${NAME}.summit.bed
+echo "Preparing data form clipper for ${NAME}..."
+zcat ${ROOTDIR}/clipper/${ID}/${NAME}.peak.bed.gz | awk -F"\t" '{if ($6=="+") print $1"\t"$2"\t"$2+1"\t"$4"\t"$5"\t"$6; else print $1"\t"$3-1"\t"$3"\t"$4"\t"$5"\t"$6}' > ${ROOTDIR}/clipper/${ID}/${NAME}.summit.bed
 done
 
+echo "Calculating mapping depth..."
+for NAME in ${REPEAT};
+do
+echo "Calculating mapping depth for ${NAME}..."
+samtools depth -l 10 ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.bamAligned.sortedByCoord.out.bam > ${ROOTDIR}/STAR/${ID}/${NAME}.10.coverage
+done
 
-
-Rscript ../find_peak.R \
-/home/liuhongjiang/projects/CLIP/WS_211115/clipper/211214_2/WS015.summit.bed \
-/home/liuhongjiang/projects/CLIP/WS_211115/STAR/211214/WS015.positive.10.coverage \
+echo "Calling peaks..."
+for NAME in ${REPEAT};
+do
+echo "Calling peaks for ${NAME}..."
+Rscript ../R/peak.R \
+${ROOTDIR}/clipper/${ID}/${NAME}.summit.bed \
+${ROOTDIR}/STAR/${ID}/${NAME}.10.coverage \
 1.4 \
-/home/liuhongjiang/projects/CLIP/WS_211115/find_peak/WS015.1.4.summits.filtered.bed
+${ROOTDIR}/peak/${ID}/${NAME}.GRIP.peak.bed
+done
+
