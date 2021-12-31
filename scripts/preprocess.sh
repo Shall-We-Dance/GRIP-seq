23335be3de8dd954ccc3fe71543319f39695f81a
Liu <91458979+Shall-We-Dance@users.noreply.github.com>
2021-12-30 19:37:46 -0800

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -86,7 +86,7 @@ echo "Indexing and calculating depth..."
 for NAME in ${REPEAT};
 do
 samtools index -b ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.bamAligned.sortedByCoord.out.bam;
-samtools depth -l 10 ${ROOTDIR}/find_peak/${NAME}.bamAligned.sortedByCoord.out.bam > ${ROOTDIR}/find_peak/${NAME}.10.coverage
+samtools depth -l 10 ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.bamAligned.sortedByCoord.out.bam > ${ROOTDIR}/find_peak/${NAME}.10.coverage
 done
 echo "Finished!"
 exit 0
