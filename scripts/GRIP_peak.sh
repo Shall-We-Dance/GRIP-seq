4d3e9cde4ca6805a9e0389b0434a04de89859c24
Liu <91458979+Shall-We-Dance@users.noreply.github.com>
2021-12-31 13:51:15 -0800

    Update GRIP_peak.sh

--- scripts/GRIP_peak.sh
+++ scripts/GRIP_peak.sh
@@ -4,3 +4,11 @@ do
 #call_summits
 zcat ${ROOTDIR}/${NAME}.peak.bed.gz |  awk -F"\t" '{if ($6=="+") print $1"\t"$2"\t"$2+1"\t"$4"\t"$5"\t"$6; else print $1"\t"$3-1"\t"$3"\t"$4"\t"$5"\t"$6}' > ${ROOTDIR}/${NAME}.summit.bed
 done
+
+
+
+Rscript ../find_peak.R \
+/home/liuhongjiang/projects/CLIP/WS_211115/clipper/211214_2/WS015.summit.bed \
+/home/liuhongjiang/projects/CLIP/WS_211115/STAR/211214/WS015.positive.10.coverage \
+1.4 \
+/home/liuhongjiang/projects/CLIP/WS_211115/find_peak/WS015.1.4.summits.filtered.bed
