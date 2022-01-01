69d25aa4a7f8a21b44c921ad74a5db25cfa8572a
Liu <91458979+Shall-We-Dance@users.noreply.github.com>
2022-01-01 00:20:22 -0800

    Update peaks.R

--- peaks/peaks.R
+++ peaks/peaks.R
@@ -13,7 +13,6 @@ args <- commandArgs(trailingOnly=TRUE)
 raw_input <- args[1]
 ref_input <- args[2]
 threshhold <- args[3]
-threshhold_peak <- args[3]
 output_file_name <- args[4]
 
 raw <- read.table(raw_input,header=F)
@@ -73,46 +72,64 @@ for(i in 1:nrow(prepeaks)){
 
 colnames(prepeaks) <- c("chr","start","end","name","pval","strain","p1","p2","p3","p4","p5","p6","p7","p8","p9","p10","p11","p12","p13","p14","p15","p16")
 print("Finding depth...")
+p5 <- progress_bar$new(total = 16)
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p1"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p2"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p3"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p4"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p5"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p6"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p7"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p8"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p9"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p10"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p11"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p12"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p13"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p14"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p15"="postion"))
+p5$tick()
 prepeaks <- left_join(prepeaks,ref,by=c("chr","p16"="postion"))
+p5$tick()
 
 prepeaks[is.na(prepeaks)] <- 5
 
 p4 <- progress_bar$new(total = nrow(prepeaks))
 print("Finding peaks (2nd trun)...")
+# To find out the biggest different point
 for(i in 1:nrow(prepeaks)){
   p4$tick()
+  high_value <- 0
   for(j in 23:37){
-    if(prepeaks[i,j] / prepeaks[i,j+1] > threshhold_peak){
-      if(prepeaks[i,6] == "-"){
-        temp_peak <- data.frame(prepeaks[i,1],(prepeaks[i,j-16]-1),(prepeaks[i,j+1-16]-1),prepeaks[i,4],prepeaks[i,5],prepeaks[i,6])
-        colnames(temp_peak) <- c("chr","start","end","name","pval","strain")
-        peaks <- bind_rows(peaks,temp_peak)
-        break
-      }
-      if(prepeaks[i,6] == "+"){
-        temp_peak <- data.frame(prepeaks[i,1],prepeaks[i,j+1-16],prepeaks[i,j-16],prepeaks[i,4],prepeaks[i,5],prepeaks[i,6])
-        colnames(temp_peak) <- c("chr","start","end","name","pval","strain")
-        peaks <- bind_rows(peaks,temp_peak)
-        break
-      }
+    if(prepeaks[i,j] / prepeaks[i,j+1] > high_value){
+      high_value <- prepeaks[i,j] / prepeaks[i,j+1]
+      x_high <- j
     }
   }
+  if(prepeaks[i,6] == "-"){
+    temp_peak <- data.frame(prepeaks[i,1],(prepeaks[i,j-16]-1),(prepeaks[i,j+1-16]-1),prepeaks[i,4],prepeaks[i,5],prepeaks[i,6])
+  }
+  if(prepeaks[i,6] == "+"){
+    temp_peak <- data.frame(prepeaks[i,1],prepeaks[i,j+1-16],prepeaks[i,j-16],prepeaks[i,4],prepeaks[i,5],prepeaks[i,6])
+  }
+  colnames(temp_peak) <- c("chr","start","end","name","pval","strain")
+  peaks <- bind_rows(peaks,temp_peak)
 }
+
 print("Successed...")
 print("Writing into file...")
 print("Finished...")
