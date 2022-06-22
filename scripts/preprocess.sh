71d8d55d2dd51ea29358f18b5d37a2e041c1f3f4
Liu <91458979+Shall-We-Dance@users.noreply.github.com>
2022-06-22 16:48:16 +0800

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#Preprocess
+# Preprocess
 
-#GRIP-seq pipeline
-#Author: Hongijang Liu
-#Email: hongjiang.liu@ucsf.edu
+# GRIP-seq pipeline
+# Author: Hongijang Liu
+# Email: hongjiang.liu@ucsf.edu
 
 # Exit the script if an error happens
 set -e
@@ -29,15 +29,15 @@ echo "ID: ${ID} ..."
 echo "Using CPU threads: ${THREAD} ..."
 
 mkdir -p ${ROOTDIR}/fastp/${ID}
-mkdir -p ${ROOTDIR}/cutadapt/${ID}
 mkdir -p ${ROOTDIR}/STAR/${ID}
 
 echo "Preprocessing the fastq files..."
 
 for NAME in ${REPEAT};
 do
-#fastp
+# fastp
 echo "Preprocessing the fastq files for ${NAME}..."
+## Read length filter
 fastp -i ${ROOTDIR}/raw_data/${NAME}_L003_R1_001.fastq.gz \
 -I ${ROOTDIR}/raw_data/${NAME}_L003_R2_001.fastq.gz \
 -o ${ROOTDIR}/fastp/${ID}/${NAME}_R1_length.fastq.gz \
@@ -47,7 +47,9 @@ fastp -i ${ROOTDIR}/raw_data/${NAME}_L003_R1_001.fastq.gz \
 --thread ${THREAD} -Q -A \
 --length_required 101 \
 --length_limit 101
+## quality control and 
 
+illumina adapters
 fastp -i ${ROOTDIR}/fastp/${ID}/${NAME}_R1_length.fastq.gz \
 -I ${ROOTDIR}/fastp/${ID}/${NAME}_R2_length.fastq.gz \
 -o ${ROOTDIR}/fastp/${ID}/${NAME}_R1_fastp_dedup_adapter.fastq.gz \
@@ -57,21 +59,27 @@ fastp -i ${ROOTDIR}/fastp/${ID}/${NAME}_R1_length.fastq.gz \
 --thread ${THREAD} \
 --dedup
 
-#cutadapt
-cutadapt -j ${THREAD} -q 10,10 -u 12 -u -10 -o ${ROOTDIR}/cutadapt/${ID}/${NAME}_R1_cutadapt_fastp_dedup_adapter.fastq.gz ${ROOTDIR}/fastp/${ID}/${NAME}_R1_fastp_dedup_adapter.fastq.gz;
-cutadapt -j ${THREAD} -q 10,10 -u 10 -u -12 -o ${ROOTDIR}/cutadapt/${ID}/${NAME}_R2_cutadapt_fastp_dedup_adapter.fastq.gz ${ROOTDIR}/fastp/${ID}/${NAME}_R2_fastp_dedup_adapter.fastq.gz;
+## cut GRIP-seq own adapters
+fastp -i ${ROOTDIR}/fastp/${ID}/${NAME}_R1_fastp_dedup_adapter.fastq.gz \
+-I ${ROOTDIR}/fastp/${ID}/${NAME}_R2_fastp_dedup_adapter.fastq.gz \
+-o ${ROOTDIR}/fastp/${ID}/${NAME}_R1_fastp_final.fastq.gz \
+-O ${ROOTDIR}/fastp/${ID}/${NAME}_R2_astp_final.fastq.gz \
+-h ${ROOTDIR}/fastp/${ID}/${NAME}_fastp_final.html \
+-j ${ROOTDIR}/fastp/${ID}/${NAME}_fastp_final.json \
+--thread ${THREAD} \
+-A \
+-f 12 -t 10 -F 10 -T 12
 done
 
 #STAR
-echo "Mapping..."
-ulimit -n 65535
+echo "Mapping reads to genome..."
 for NAME in ${REPEAT};
 do
 echo "Mapping for ${NAME}..."
 STAR  --runThreadN ${THREAD} \
 --genomeDir  ${GENOMEDIR_STAR} \
 --readFilesCommand zcat \
---readFilesIn  ${ROOTDIR}/cutadapt/${ID}/${NAME}_R2_cutadapt_fastp_dedup_adapter.fastq.gz \
+--readFilesIn  ${ROOTDIR}/fastp/${ID}/${NAME}_R2_astp_final.fastq.gz \
 --outFileNamePrefix ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.bam \
 --outSAMtype BAM SortedByCoordinate ;
 done
