542b67ed5e01b590327e545e9c4c1830288e8220
Liu <91458979+Shall-We-Dance@users.noreply.github.com>
2022-06-22 16:50:24 +0800

    Update preprocess.sh

--- scripts/preprocess.sh
+++ scripts/preprocess.sh
@@ -7,6 +7,7 @@
 # Email: hongjiang.liu@ucsf.edu
 
 # Exit the script if an error happens
+
 set -e
 
 if [[ $# -eq 0 ]]; then
@@ -35,9 +36,13 @@ echo "Preprocessing the fastq files..."
 
 for NAME in ${REPEAT};
 do
+
 # fastp
+
 echo "Preprocessing the fastq files for ${NAME}..."
+
 ## Read length filter
+
 fastp -i ${ROOTDIR}/raw_data/${NAME}_L003_R1_001.fastq.gz \
 -I ${ROOTDIR}/raw_data/${NAME}_L003_R2_001.fastq.gz \
 -o ${ROOTDIR}/fastp/${ID}/${NAME}_R1_length.fastq.gz \
@@ -47,9 +52,9 @@ fastp -i ${ROOTDIR}/raw_data/${NAME}_L003_R1_001.fastq.gz \
 --thread ${THREAD} -Q -A \
 --length_required 101 \
 --length_limit 101
-## quality control and 
 
-illumina adapters
+## quality control and cut illumina adapters
+
 fastp -i ${ROOTDIR}/fastp/${ID}/${NAME}_R1_length.fastq.gz \
 -I ${ROOTDIR}/fastp/${ID}/${NAME}_R2_length.fastq.gz \
 -o ${ROOTDIR}/fastp/${ID}/${NAME}_R1_fastp_dedup_adapter.fastq.gz \
@@ -60,6 +65,7 @@ fastp -i ${ROOTDIR}/fastp/${ID}/${NAME}_R1_length.fastq.gz \
 --dedup
 
 ## cut GRIP-seq own adapters
+
 fastp -i ${ROOTDIR}/fastp/${ID}/${NAME}_R1_fastp_dedup_adapter.fastq.gz \
 -I ${ROOTDIR}/fastp/${ID}/${NAME}_R2_fastp_dedup_adapter.fastq.gz \
 -o ${ROOTDIR}/fastp/${ID}/${NAME}_R1_fastp_final.fastq.gz \
@@ -72,10 +78,14 @@ fastp -i ${ROOTDIR}/fastp/${ID}/${NAME}_R1_fastp_dedup_adapter.fastq.gz \
 done
 
 #STAR
+
 echo "Mapping reads to genome..."
+
 for NAME in ${REPEAT};
 do
+
 echo "Mapping for ${NAME}..."
+
 STAR  --runThreadN ${THREAD} \
 --genomeDir  ${GENOMEDIR_STAR} \
 --readFilesCommand zcat \
@@ -85,11 +95,14 @@ STAR  --runThreadN ${THREAD} \
 done
 
 echo "Indexing and calculating depth..."
+
 for NAME in ${REPEAT};
 do
 samtools index -b ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.bamAligned.sortedByCoord.out.bam;
 samtools depth ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.bamAligned.sortedByCoord.out.bam > ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.coverage
 cat ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.coverage | awk '$3 > 10 {print $0}' > ${ROOTDIR}/STAR/${ID}/${NAME}/${NAME}.positive.10.coverage
 done
+
 echo "Finished!"
+
 exit 0
